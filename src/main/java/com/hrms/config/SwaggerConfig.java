758a59e4275dbebbf0d525e8b991ee9f0632dede
Levent Suncar <80010942+leventsuncar@users.noreply.github.com>
2021-05-26 20:33:30 +0300

    Update SwaggerConfig.java

--- src/main/java/com/hrms/config/SwaggerConfig.java
+++ src/main/java/com/hrms/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.hrms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,10 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Human Resources Management System API Reference")
+                .build();
+    }
 
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.hrms"))
                 .build();
